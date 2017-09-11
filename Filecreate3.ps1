1c94be9e96dbcb3a96b399dbc8314be3eff70f2b
Lalums <31470639+Lalums@users.noreply.github.com>
2017-09-11 22:08:44 +0530

    Add files via upload

---
+++ Filecreate3.ps1
@@ -0,0 +1,16 @@
+﻿configuration createFile
+    {
+Param (
+[String[]]$computername
+)
+    
+ Node @($Computername)
+       
+        {
+          File CreateFile {
+            Type = 'Directory'
+            DestinationPath = 'C:\Ratheesh'
+            Ensure = "Present"
+            }
+        }
+    }
