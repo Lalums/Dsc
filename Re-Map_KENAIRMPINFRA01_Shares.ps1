88b354dd9f88919ef784730bfee8336649ff8da7
Lalums <31470639+Lalums@users.noreply.github.com>
2017-11-10 17:05:47 +0530

    Add files via upload

---
+++ Re-Map_KENAIRMPINFRA01_Shares.ps1
@@ -0,0 +1,57 @@
+
+$NewserverFQDN = "KENAIRMPFL01.ey.net"
+$OldServerFQDN = "KENAIRMPINFRA01.ey.net"
+
+
+$ErrorActionPreference = ‘SilentlyContinue’
+$Networkdrives = Get-WmiObject -Class Win32_MappedLogicalDisk | select Name, ProviderName
+$Cred1 = $(Get-Credential).GetNetworkCredential()
+
+#Copying the existing network drive information to temp folder
+$Networkdrives | out-file -LiteralPath "C:\Users\gunnjo2\Documents\SCS - TA and MS Team\0 - As Is File Server Mirgation\Drive Mapping Scripts\mapdrives.txt" -Append
+$new = "$NewserverFQDN".Split('.')
+$newServer = $new[0]
+$old = "$OldServerFQDN".Split('.')
+$oldserver = $old[0]
+if($Networkdrives -ne "")
+{
+       foreach ($Networkdrive in $Networkdrives)
+
+      {
+        $newmap = ""
+        $split= ""
+        $Name = $Networkdrive.Name
+        $Location= $Networkdrive.ProviderName
+        $split= "$location".Split('\\\\')
+
+       if($split[2] -eq $OldServerFQDN)
+        {
+          net use $name /delete
+          $newmap = $Location -replace $OldServerFQDN,$NewserverFQDN
+		  net use $name $newmap /persistent:YES $Cred1.Password /USER:$($Cred1.domain)\$($Cred1.UserName)
+          
+            #$Net = $(New-Object -ComObject WScript.Network)
+            #$Net.MapNetworkDrive($name,$newmap)
+            #$Net.objNetwork.EnumNetworkDrives
+
+       }
+       if($split[2] -eq $oldserver)
+         {
+          net use $name /delete
+          $newmap = $Location -replace $oldserver,$NewserverFQDN
+		  net use $name $newmap /persistent:YES $Cred1.Password /USER:$($Cred1.domain)\$($Cred1.UserName)
+          
+            #$Net = $(New-Object -ComObject WScript.Network)
+            #$Net.MapNetworkDrive($name,$newmap)
+            #$Net.objNetwork.EnumNetworkDrives 
+          }
+          else{ Write-Host "fileserver not match" }
+
+      }  
+      taskkill /f /IM explorer.exe
+
+      Start-Sleep -Seconds 5
+      explorer.exe                              
+      }       	
+
+
